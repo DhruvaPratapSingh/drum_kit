17872c9c923a5779994a9bab80e0ecb271f73b44
DhruvaPratapSingh <ds8987765@gmail.com>
2023-07-04 17:29:06 +0530

    commit

---
+++ java.js
@@ -0,0 +1,22 @@
+// document.querySelector("button").addEventListener("click",handclick);
+// function handclick(){
+//     alert("i got clicked");
+// }
+for(let i=0;i<document.querySelectorAll(".drum").length;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    // let audio = new Audio('snare.mp3');
+    // audio.play();
+    //  this.style.color="white";
+});
+}
+// function add(a,b){
+//     return a+b;
+// }
+// function multiply(a,b){
+//     return a*b;
+// }
+// function calc(a,b,operator){
+//     return operator(a,b);
+// }
+// document.write(calc(3,4,multiply));
+// document.write(calc(3,4,add));
